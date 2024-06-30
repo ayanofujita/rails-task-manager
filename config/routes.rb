b63c5073fffccd04ed389ae453272087cddb65c3
Ayano Fujita <cathy.ayano@gmail.com>
2024-06-30 11:05:46 +0900

    refactored

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,17 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  get "tasks", to: "tasks#all"
+  # get "tasks", to: "tasks#all"
 
-  post "tasks", to: "tasks#create"
-  get "tasks/new", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
+  # get "tasks/new", to: "tasks#new"
 
-  get "tasks/:id/edit", to: "tasks#edit", as: :edit
-  patch "tasks/:id", to: "tasks#update"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  # patch "tasks/:id", to: "tasks#update"
 
-  get "tasks/:id", to: "tasks#show", as: :task
+  # get "tasks/:id", to: "tasks#show", as: :task
 
-  delete "tasks/:id", to: "tasks#destroy"
+  # delete "tasks/:id", to: "tasks#destroy"
+
+  resources :tasks
 end
